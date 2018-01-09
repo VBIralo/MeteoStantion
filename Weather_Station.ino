0667bb58959abaafc6da96987771e9cba3102b2b
Vlad Biralo <mr.biralo@yandex.ru>
2018-01-10 01:08:27 +0300

    FIX DewPoint, ADD HeatIndex & many innovations :3

--- Weather_Station.ino
+++ Weather_Station.ino
@@ -3,92 +3,123 @@
 #include <BlynkSimpleEsp8266.h>
 #include "Seeed_BME280.h"
 #include <Wire.h>
+#include "config.h"
 
+// Init BME & WIFI
 BME280 bme280;
+float temp(NAN), humi(NAN), pres(NAN);
+float dp(NAN), hi(NAN);
 WiFiClient client;
 
-char auth[] = "Blynk_api_key";
-char ssid[] = "WiFi SSID(name)";
-char pass[] = "WiFi PASSWORD";
-char server[] = "api.thingspeak.com";
-char api_key[] = "THINGSPEAK_WRITE_KEY";
+ADC_MODE(ADC_VCC);
+long vcc = 0;
 
 void setup(){
-  Wire.begin();
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
-  WiFi.begin(ssid, pass);
-  
-  while (WiFi.status() != WL_CONNECTED){
-  Serial.println("WiFi connected");
-  }
+  Serial.println("Waking up");
+  delay(1000);
+  // Init BME
+  Wire.begin();
   if(!bme280.init()){
   Serial.println("Device error!");
+  }
+  // Init Blynk
+  Blynk.begin(BLYNK_KEY, WIFI_SSID, WIFI_PASS);
+  // Init WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED){
+  Serial.print("Connecting WiFi:");
+      Serial.println(WIFI_SSID);
+  }
+  /// Input Voltage
+  vcc = ESP.getVcc();
+
+  if (vcc <= BATT_CRT){
+    Serial.print("The battery charge is critical, go to sleep!");
+    goDeepSleep();
   }
 }
 
 void loop(){
   Blynk.run();
-
-  float temp = bme280.getTemperature();
-  float pressure = (bme280.getPressure() / 100.0)*0.7500637; //hPa
-  float humidity = bme280.getHumidity();
-  double dewpoint = dewPoint(temp, humidity);
   
-  //getting and sending temperature
-  Serial.print("Температура: ");
+  // Getting temperature & pressure & humidity
+  float temp = bme280.getTemperature();
+  float pres = (bme280.getPressure() / 100.0)*0.7500637; //hPa
+  float humi = bme280.getHumidity();
+
+  // Calculate HeatIndex and DewPoint
+  float dp = computeDewPoint(temp, humi);
+  float hi = computeHeatIndex(temp, humi);
+  
+  // Getting and sending temperature
+  Serial.print("Temperature:");
   Serial.print(temp);
   Serial.println("C");
   Blynk.virtualWrite(0, temp); // virtual pin 0
 
-  //getting and sending pressure
-  Serial.print("Давление:    ");
-  Serial.print(pressure);
-  Serial.println("гПа");
-  Blynk.virtualWrite(1, pressure); // virtual pin 1
+  // Getting and sending pressure
+  Serial.print("Pressure:   ");
+  Serial.print(pres);
+  Serial.println("hPa");
+  Blynk.virtualWrite(1, pres); // virtual pin 1
 
-  //getting and sending humidity
-  Serial.print("Влажность:   ");
-  Serial.print(humidity);
+  // Getting and sending humidity
+  Serial.print("Humidity:   ");
+  Serial.print(humi);
   Serial.println("%");
-  Blynk.virtualWrite(2, humidity); // virtual pin 2
+  Blynk.virtualWrite(2, humi); // virtual pin 2
 
-  //getting and sending dewpoint
-  Serial.print("Точка росы:  ");
-  Serial.print(dewpoint);
+  // Getting and sending dewpoint
+  Serial.print("DewPoint:   ");
+  Serial.print(dp);
   Serial.println("C");
-  Blynk.virtualWrite(3, dewpoint); // virtual pin 3
+  Blynk.virtualWrite(3, dp); // virtual pin 3
 
-  postData();
+  // Getting and sending heatindex
+  Serial.print("HeatIndex:  ");
+  Serial.print(hi);
+  Serial.println("C");
+  Blynk.virtualWrite(4, hi); // virtual pin 4
+
+  // Getting and sending VCC
+  Serial.print("Vcc:        ");
+  Serial.print(vcc);
+  Serial.println("mV");
+  Blynk.virtualWrite(5, hi); // virtual pin 5
+
+  postData(temp, pres, humi, dp, hi, vcc);
   
-  ESP.deepSleep(300e6); // 60e6 == 1min // 300e6 == 5 min
+  Serial.println("Work is done. Going back to sleep.");
+  goDeepSleep();
 }
 
-long postData() {
-  float temp = bme280.getTemperature();
-  float pressure = (bme280.getPressure() / 100.0)*0.7500637;
-  float humidity = bme280.getHumidity();
-  float dewpoint = dewPoint(temp, humidity);
-  
-  if (client.connect(server,80)) {
+long postData(float temp, float pres, float humi, float dewpoint, float heatindex, float vcc) {
+  if (client.connect(TS_HOST,80)) {
   Serial.println("Connect to ThingSpeak - OK"); 
   Serial.println("////////////////////////////////////////////");
 
   String dataToThingSpeak = "";
   dataToThingSpeak+="GET /update?api_key=";
-  dataToThingSpeak+=api_key;
+  dataToThingSpeak+=TS_KEY;
    
   dataToThingSpeak+="&field1=";
   dataToThingSpeak+=String(temp);
 
   dataToThingSpeak+="&field2=";
-  dataToThingSpeak+=String(humidity);
+  dataToThingSpeak+=String(humi);
 
   dataToThingSpeak+="&field3=";
-  dataToThingSpeak+=String(pressure);
+  dataToThingSpeak+=String(pres);
   
   dataToThingSpeak+="&field4=";
   dataToThingSpeak+=String(dewpoint);
+
+  dataToThingSpeak+="&field5=";
+  dataToThingSpeak+=String(heatindex);
+
+  dataToThingSpeak+="&field6=";
+  dataToThingSpeak+=String(vcc);
    
   dataToThingSpeak+=" HTTP/1.1\r\nHost: a.c.d\r\nConnection: close\r\n\r\n";
   dataToThingSpeak+="";
@@ -105,11 +136,68 @@ long postData() {
 }
 }
 
-long dewPoint(float temp, float humidity) {
-  double b = 17.62;
-  double c = 243.50;
-  double gamma = log(humidity/100) + (b*temp / (c+temp));
-  double dewpoint = c*gamma / (b-gamma);
+void goDeepSleep(){
+  Serial.print("Deep Sleep (us):");
+  long sleepTime = (INTERVAL_NRM - millis()) * 1000;
+  if (vcc <= BATT_LOW){
+    sleepTime = (INTERVAL_LOW - millis()) * 1000;
+  }
+  if (sleepTime < 1){
+    sleepTime = 1;
+  }
+
+  Serial.println(sleepTime);
+  ESP.deepSleep(sleepTime);
+}
+
+long computeDewPoint(float temp, float humi) {
+  // Equations courtesy of Brian McNoldy
+  // http://andrew.rsmas.miami.edu;
+
+  float dewpoint = NAN;
+  double b = 17.625;
+  double c = 243.04;
+
+  dewpoint = c * (log(humi/100.0) + ((b * temp)/(c + temp)))
+  /(b - log(humi/100.0) - ((b * temp)/(c + temp)));
 
   return dewpoint;
+}
+
+float computeHeatIndex(float temp, float humi) {
+  // Using both Rothfusz and Steadman's equations
+  // http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+
+  float heatindex;
+  temp = convertCtoF(temp);
+
+  heatindex = 0.5 * (temp + 61.0 + ((temp - 68.0) * 1.2) + (humi * 0.094));
+
+  if (heatindex > 79) {
+    heatindex = -42.379 +
+             2.04901523 * temp +
+            10.14333127 * humi +
+            -0.22475541 * temp*humi +
+            -0.00683783 * pow(temp, 2) +
+            -0.05481717 * pow(humi, 2) +
+             0.00122874 * pow(temp, 2) * humi +
+             0.00085282 * temp*pow(humi, 2) +
+            -0.00000199 * pow(temp, 2) * pow(humi, 2);
+
+    if ((humi < 13) && (temp >= 80.0) && (temp <= 112.0)){
+      heatindex -= ((13.0 - humi) * 0.25) * sqrt((17.0 - abs(temp - 95.0)) * 0.05882);
+    } else if ((humi > 85.0) && (temp >= 80.0) && (temp <= 87.0)){
+      heatindex += ((humi - 85.0) * 0.1) * ((87.0 - temp) * 0.2);
+    }
+  }
+
+  return convertFtoC(heatindex);
+}
+
+float convertCtoF(float c) {
+  return c * 1.8 + 32;
+}
+
+float convertFtoC(float f) {
+  return (f - 32) * 0.55555;
 }
---
+++ config.h
@@ -0,0 +1,19 @@
+// WiFi setting
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASS "WIFI_PASS"
+
+// Blynk setting
+#define BLYNK_KEY "BLYNK_API_KEY"
+
+// ThingSpeak setting
+#define TS_HOST "api.thingspeak.com"
+#define TS_PORT 80
+#define TS_KEY "THINGSPEAK_API_KEY"
+
+// DeepSleep interval
+#define INTERVAL_NRM 300000 // == 5 min /// normal wake up interval
+#define INTERVAL_LOW 900000 // == 15 min // wake up interval when low battery
+
+// Setup 1x18650 battery
+#define BATT_LOW 2300 // low battery voltage
+#define BATT_CRT 2200 // critical battery voltage
